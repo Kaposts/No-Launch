b3bebbde11b461a96dcb3cbfb00d3765c487e33a
Cabage <kaposts.artis1@gmail.com>
2025-09-14 16:10:14 +0000

    bug fix for round-effects and some animations

--- core/signal_bus.gd
+++ core/signal_bus.gd
@@ -5,6 +5,7 @@ signal activate_card(card: Card)
 signal deactivate_card(card: Card)
 signal update_energy(value: int)
 signal round_effect_activate(description: String)
+signal round_effect_hide()
 
 signal update_hand()
 signal draw_card(card_data: CardData)
--- ui/player_ui.gd
+++ ui/player_ui.gd
@@ -4,6 +4,7 @@ func _ready():
 	SignalBus.update_energy.connect(_on_update_energy)
 	SignalBus.update_hand.connect(_on_update_hand)
 	SignalBus.round_effect_activate.connect(_on_round_effect_activate)
+	SignalBus.round_effect_hide.connect(on_round_effect_hide)
 
 func _on_update_energy(value):
 	$energy.text = "Executive Power: " + str(value) +"/"+ str(Global.max_energy)
@@ -15,6 +16,9 @@ func _on_play_turn_pressed() -> void:
 
 func _on_play_turn_2_pressed() -> void:
 	SignalBus.end_round.emit()
+
+func on_round_effect_hide():
+	$round_effect/AnimationPlayer.play("hide")
 
 func _on_round_effect_activate(description: String):
 	$round_effect.show()
